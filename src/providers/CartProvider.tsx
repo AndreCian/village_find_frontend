74e419f2d29a89852cf35f921cdd4f2515461eae
Johny <wowcardjohnygiang@gmail.com>
2024-06-06 10:26:50 -0400

    Fix cart provider

--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -181,7 +181,7 @@ export function CartProvider({ children }: ICartProviderProps) {
         setCartItems(response || []);
       }
     });
-  }, [isLogin, account?.profile._id, guestID]);
+  }, [isLogin, account, guestID]);
 
   useEffect(() => {
     setCartItemCount(cartItems.reduce(
